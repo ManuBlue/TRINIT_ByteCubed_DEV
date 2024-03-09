16fa9b045d2a34a01c3c7804c483a3050d9c5602
ManuBlue <161926227+ManuBlue@users.noreply.github.com>
2024-03-10 03:23:24 +0530

    Add files via upload

--- models/doubt.js
+++ models/doubt.js
@@ -1,1 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const doubtSchema = new Schema({
+  mydoubt: {
+    type: String,
+    required: true,
+  },
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: false,
+  },
+  answer : {
+    type: String,
+    required: false,
+  },
+
+}, { timestamps: true });
+
+const Doubt = mongoose.model('Doubt', doubtSchema);
+module.exports = Doubt;
