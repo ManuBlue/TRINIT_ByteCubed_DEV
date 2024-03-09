51f784a5d84baa194d52fec2ca8593def9c568c6
ManuBlue <161926227+ManuBlue@users.noreply.github.com>
2024-03-09 09:23:04 +0530

    Add files via upload

--- project/models/studentaccount.js
+++ project/models/studentaccount.js
@@ -1,1 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const accountSchema1 = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  email : {
+    type: String,
+    required: true,
+  },
+  phno : {
+    type: String,
+    required: true,
+  },
+  classes : {
+    type : Object,
+    required :false,
+  },
+
+}, { timestamps: true });
+
+const StudentAccount = mongoose.model('StudentAccount', accountSchema1);
+module.exports = StudentAccount;
